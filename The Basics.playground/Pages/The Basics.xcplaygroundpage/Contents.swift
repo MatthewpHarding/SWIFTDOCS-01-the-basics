ea294fa1d559cd4859ee4de32d36298ee8739820
Matthew Harding <matthewpharding@gmail.com>
2022-10-22 11:48:10 +0100

    Resolved some run time errors with the Xcode playground, adding run time error comments so that the playground can be executed by default

--- The Basics.playground/Pages/The Basics.xcplaygroundpage/Contents.swift
+++ The Basics.playground/Pages/The Basics.xcplaygroundpage/Contents.swift
@@ -554,19 +554,22 @@ do {
 //:
 //: You write an assertion by calling the `assert(_:_:file:line:)` function from the Swift standard library. You pass this function an expression that evaluates to `true` or `false` and a message to display if the result of the condition is false. For example:
 let age = -3
-assert(age >= 0, "A person's age can't be less than zero.")
+// ⛔️ Run time error: uncomment to view
+//assert(age >= 0, "A person's age can't be less than zero.")
 // This assertion fails because -3 isn't >= 0.
 //: In this example, code execution continues `if age >= 0` evaluates to `true`, that is, if the value of age is nonnegative. If the value of age is negative, as in the code above, then `age >= 0` evaluates to `false`, and the assertion fails, terminating the application.
 //:
 //: You can omit the assertion message—for example, when it would just repeat the condition as prose.
-assert(age >= 0)
+// ⛔️ Run time error: uncomment to view
+//assert(age >= 0)
 //: If the code already checks the condition, you use the `assertionFailure(_:file:line:)` function to indicate that an assertion has failed. For example:
 if age > 10 {
     print("You can ride the roller-coaster or the ferris wheel.")
 } else if age >= 0 {
     print("You can ride the ferris wheel.")
 } else {
-    assertionFailure("A person's age can't be less than zero.")
+// ⛔️ Run time error: uncomment to view
+//    assertionFailure("A person's age can't be less than zero.")
 }
 //: ### Enforcing Preconditions
 //:
